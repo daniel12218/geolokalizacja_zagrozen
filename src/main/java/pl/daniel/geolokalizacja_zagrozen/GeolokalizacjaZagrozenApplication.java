02d0172a39fcafc7b7c68e8388df43e21de796bd
Daniel Sacha <daniel181222@onet.pl>
2020-03-19 17:21:34 +0100

    initial project version

---
+++ src/main/java/pl/daniel/geolokalizacja_zagrozen/GeolokalizacjaZagrozenApplication.java
@@ -0,0 +1,13 @@
+package pl.daniel.geolokalizacja_zagrozen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolokalizacjaZagrozenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeolokalizacjaZagrozenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/daniel/geolokalizacja_zagrozen/GeolokalizacjaZagrozenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.daniel.geolokalizacja_zagrozen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolokalizacjaZagrozenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
